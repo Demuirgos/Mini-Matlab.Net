01f328567b58df63f86e85cf01ffb2e8a8563d1c
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-01-23 07:25:02 -0800

    Add files via upload

---
+++ mini_MATLAB/ConsoleApplication9/untested_class1.vb
@@ -0,0 +1,89 @@
+Public Class function_OP
+    Delegate Function f(ByVal x As Decimal) As Decimal
+    Delegate Function f2(ByVal x As Decimal, t() As Decimal, i As Integer) As Decimal
+    Delegate Function f_mv(ByVal a() As Decimal) As Decimal
+    Public Shared Function integrate(ByVal g As f, a As Decimal, b As Decimal, n As Integer, Optional m As Integer = 3) As Decimal
+        Dim c As Decimal = 0
+        Dim h As Double = (b - a) / n
+        Dim h2 As Double = 2 / m
+        For i = 0 To n - 1
+            For j = 0 To m
+                c += (h / 2) * intgrl2(AddressOf lag, h2, i) * g((h / 2) * (j*h2-1) + (2*a+h*(2*i+1) / 2))
+            Next
+        Next
+        'Return function_OP.rnd(c, 7)
+        Return c
+    End Function
+    Public Shared Function lag(x As Decimal, h2 As Decimal, n As Integer) As Decimal
+        Dim c As Decimal = 1
+        For i = 0 To t.Length - 1
+            If Not i = n Then
+                c *= (x - (i*h2-1) / (h2*(n-i)
+            End If
+        Next
+        Return c
+    End Function
+    Public Shared Function intgrl2(ByVal g As f2, h2 As Decimal, n As Integer, Optional j As Integer = 3) As Decimal
+        Dim c As Decimal
+        Dim x(0) As Double
+        Dim k As Integer = 10000 * (t.Length - 1)
+        Dim h As Double = 2 / k
+        ReDim x(k)
+        For i = 0 To k
+            x(i) = -1 + i * h
+        Next
+        If j = 1 Then
+            For i = 0 To k - 1
+                c += (h / 2) * (g(h(2*i+1)-2) / 2, h2, n))
+            Next
+        ElseIf j = 2 Then
+            For i = 0 To k - 1
+                c += (h / 2) * (g(x(i), t, n) + g(x(i + 1), t, n))
+            Next
+        Else
+            For i = 0 To k - 1
+                c += (h / 6) * (g(x(i), t, n) + 4 * g((x(i) + x(i + 1)) / 2, t, n) + g(x(i + 1), t, n))
+            Next
+        End If
+        Return c
+    End Function
+    Public Shared Function derive(ByVal g As f, x As Decimal, Optional n As Integer = 1) As Decimal
+        Dim h As Decimal = 0.0000001
+        If n = 0 Then
+            Return g(x)
+        Else
+            Return (derive(g, x + h, n - 1) - derive(g, x - h, n - 1)) / (2 * h)
+        End If
+    End Function
+
+    Public Shared Function derive(ByVal g As f_mv, x() As Decimal, m As Integer, Optional n As Integer = 1) As Decimal
+        Dim h As Decimal = 1.0E+23
+        Dim x_h() As Decimal
+        x_h = x
+        x_h(m) += 1 / h
+        If n = 0 Then
+            Return g(x)
+        Else
+            Return (derive(g, x_h, m, n - 1) - derive(g, x, m, n - 1)) * h
+        End If
+    End Function
+    Public Shared Function segment(ByVal g As f, a As Decimal, b As Decimal, Optional n As Integer = 100) As Decimal
+        Dim l As Decimal = 0
+        Dim dl As f
+        dl = Function(ByVal x As Decimal) As Decimal
+                 Dim h As Decimal = 1 / Integer.MaxValue
+                 If x + h > b Or x - h < a Then
+                     Return 1
+                 End If
+                 Return Math.Sqrt(1 + Math.Pow(((g(x + h) - g(x)) / h), 2))
+             End Function
+        l = integrate(dl, a, b, n, 3)
+        Return rnd(l, 7)
+    End Function
+    Public Shared Function img(ByVal g As f, x As Decimal) As Decimal
+        Return g(x)
+    End Function
+    Private Shared Function rnd(a As Decimal, n As Integer)
+        Return Math.Round(a * 10 ^ n) / 10 ^ n
+    End Function
+End Class
